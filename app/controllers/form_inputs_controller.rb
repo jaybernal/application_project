1f9a9c8ec657b0dc4124ef4d4d51f2a02a62fd2b
Jay Bernal <jjbernal@gmail.com>
2016-05-30 10:02:27 -0500

    updated controller routes

---
+++ app/controllers/form_inputs_controller.rb
@@ -0,0 +1,23 @@
+class FormInputsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @form_inputs = FormInput.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+end
---
+++ app/helpers/form_inputs_helper.rb
@@ -0,0 +1,2 @@
+module FormInputsHelper
+end
